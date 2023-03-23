a9b69f7bff6d48564bcdd051acdb6c5bc8dc4dc4
Novichenko Evgeniy <tnrkip@yandex.ru>
2023-03-23 10:34:26 +0300

    add ajax ul and chart

--- prostoy/views.py
+++ prostoy/views.py
@@ -25,20 +25,19 @@ def index(request):
 
         if start1 <= datetime.datetime.now().time() <= end1:
 
-            plan = 31500
             table = Table5.objects.filter(startdata=datetime.date.today(),
                                           starttime__gte=datetime.time(8),
                                           starttime__lte=datetime.time(16, 30))
             speed = speed5.objects.filter(data=datetime.date.today(),
                                           time__gte=datetime.time(8),
                                           time__lte=datetime.time(16, 30))
-            boom= bottleExplosion.objects.filter(data=datetime.date.today(),
-                                          time__gte=datetime.time(8),
-                                          time__lte=datetime.time(16, 30))
+            boom = bottleExplosion.objects.filter(data=datetime.date.today(),
+                                                  time__gte=datetime.time(8),
+                                                  time__lte=datetime.time(16, 30))
 
         elif end1 <= datetime.datetime.now().time() <= end2:
 
-            plan = 27000
+
             table = Table5.objects.filter(startdata=datetime.date.today(),
                                           starttime__gte=datetime.time(16, 29),
                                           starttime__lte=datetime.time(23, 59))
@@ -46,10 +45,10 @@ def index(request):
                                           time__gte=datetime.time(16, 29),
                                           time__lte=datetime.time(23, 59))
             boom = bottleExplosion.objects.filter(data=datetime.date.today(),
-                                          time__gte=datetime.time(16, 29),
-                                          time__lte=datetime.time(23, 59))
+                                                  time__gte=datetime.time(16, 29),
+                                                  time__lte=datetime.time(23, 59))
         else:
-            plan = 29000
+
             table = Table5.objects.filter(startdata=datetime.date.today(),
                                           starttime__gte=datetime.time(00, 00),
                                           starttime__lte=datetime.time(8, 0))
@@ -57,45 +56,19 @@ def index(request):
                                           time__gte=datetime.time(00, 00),
                                           time__lte=datetime.time(8, 0))
             boom = bottleExplosion.objects.filter(data=datetime.date.today(),
-                                          time__gte=datetime.time(16, 29),
-                                          time__lte=datetime.time(23, 59))
+                                                  time__gte=datetime.time(16, 29),
+                                                  time__lte=datetime.time(23, 59))
 
     lableChart = []
     dataChart = []
-    try:
-        avgSpeed = round(speed.aggregate(Avg('speed')).get('speed__avg'), 2)
-    except:
-        avgSpeed = 0
-    try:
-        sumProstoy = table.aggregate(Sum('prostoy')).get('prostoy__sum')
-        if (sumProstoy==None) :
-            sumProstoy='00:00'
-    except:
-        sumProstoy = 0
-    try:
-        allProduct = round(speed.aggregate(Sum('speed')).get('speed__sum') / 20, 2)
-        allProc = round(allProduct / plan * 100, 2)
-
-
-
-    except:
-        allProduct = 0
-        allProc = 0
-    try:
-        boomOut = boom.aggregate(Sum('bottle')).get('bottle__sum')
-        if (boomOut == None):
-            boomOut = 0
-    except:
-        boomOut=0
 
     for sp in speed:
         lableChart.append(str(sp.time))
         dataChart.append(sp.speed)
+
     otv_p = otv_pod.objects.all()
 
     prich = list(prichina.objects.all().values())
-
-
 
     uch = uchastok.objects.all()
     return render(request, "index.html", {
@@ -104,12 +77,6 @@ def index(request):
 
         'lableChart': lableChart,
         'dataChart': dataChart,
-
-        'sumProstoy': sumProstoy,
-        'avgSpeed': avgSpeed,
-        'allProduct': allProduct,
-        'allProc': allProc,
-        'boomOut':boomOut,
 
         'otv_p': otv_p,
         'prich': prich,
@@ -136,9 +103,9 @@ def otchet(request):
                                                   time__gte=datetime.time(0),
                                                   time__lte=datetime.time(23, 59))
                     boom = bottleExplosion.objects.filter(data__gte=form.cleaned_data["start_data"],
-                                                  data__lte=form.cleaned_data["finish_data"],
-                                                  time__gte=datetime.time(0),
-                                                  time__lte=datetime.time(23, 59))
+                                                          data__lte=form.cleaned_data["finish_data"],
+                                                          time__gte=datetime.time(0),
+                                                          time__lte=datetime.time(23, 59))
                 if form.cleaned_data["SmenaF"] == 'Smena 1':
                     table = Table5.objects.filter(starttime__gte=datetime.time(8),
                                                   starttime__lte=datetime.time(16, 30),
@@ -150,9 +117,9 @@ def otchet(request):
                                                   time__gte=datetime.time(8),
                                                   time__lte=datetime.time(16, 30))
                     boom = bottleExplosion.objects.filter(data__gte=form.cleaned_data["start_data"],
-                                                  data__lte=form.cleaned_data["finish_data"],
-                                                  time__gte=datetime.time(8),
-                                                  time__lte=datetime.time(16, 30))
+                                                          data__lte=form.cleaned_data["finish_data"],
+                                                          time__gte=datetime.time(8),
+                                                          time__lte=datetime.time(16, 30))
                 if form.cleaned_data["SmenaF"] == 'Smena 2':
                     table = Table5.objects.filter(starttime__gte=datetime.time(16, 30),
                                                   starttime__lte=datetime.time(23, 59),
@@ -164,9 +131,9 @@ def otchet(request):
                                                   time__gte=datetime.time(16, 30),
                                                   time__lte=datetime.time(23, 59))
                     boom = bottleExplosion.objects.filter(data__gte=form.cleaned_data["start_data"],
-                                                  data__lte=form.cleaned_data["finish_data"],
-                                                  time__gte=datetime.time(16, 30),
-                                                  time__lte=datetime.time(23, 59))
+                                                          data__lte=form.cleaned_data["finish_data"],
+                                                          time__gte=datetime.time(16, 30),
+                                                          time__lte=datetime.time(23, 59))
                 if form.cleaned_data["SmenaF"] == 'Smena 3':
                     table = Table5.objects.filter(starttime__gte=datetime.time(00, 00),
                                                   starttime__lte=datetime.time(8, 00),
@@ -178,9 +145,9 @@ def otchet(request):
                                                   time__gte=datetime.time(00, 00),
                                                   time__lte=datetime.time(8, 00))
                     boom = bottleExplosion.objects.filter(data__gte=form.cleaned_data["start_data"],
-                                                  data__lte=form.cleaned_data["finish_data"],
-                                                  time__gte=datetime.time(00, 00),
-                                                  time__lte=datetime.time(8, 00))
+                                                          data__lte=form.cleaned_data["finish_data"],
+                                                          time__gte=datetime.time(00, 00),
+                                                          time__lte=datetime.time(8, 00))
         # Сортировка по сменам:
 
     lableChart = []
@@ -191,7 +158,7 @@ def otchet(request):
         if (boomOut == None):
             boomOut = 0
     except:
-        boomOut=0
+        boomOut = 0
     try:
         sumProstoy = table.aggregate(Sum('prostoy')).get('prostoy__sum')
     except:
@@ -213,33 +180,95 @@ def otchet(request):
     uch = uchastok.objects.all()
     prich = prichina.objects.all()
 
+    test = prichina.objects.all()
     return render(request, "otchet.html", {
         'table': table,
         'form': form,
 
         'sumProstoy': sumProstoy,
         'avgSpeed': avgSpeed,
-        'boomOut':boomOut,
+        'boomOut': boomOut,
 
         'lableChart': lableChart,
         'dataChart': dataChart,
-
 
         'otv_p': otv_p,
         'prich': prich,
         'uch': uch,
 
+        'test': test,
+
     })
 
 
 def getData(requst):
-    table = Table5.objects.all()
 
-    return JsonResponse({"data": list(table.values())})
+    if start1 <= datetime.datetime.now().time() <= end1:
+
+        plan = 31500
+        table = Table5.objects.filter(startdata=datetime.date.today(),
+                                      starttime__gte=datetime.time(8),
+                                      starttime__lte=datetime.time(16, 30))
+        speed = speed5.objects.filter(data=datetime.date.today(),
+                                      time__gte=datetime.time(8),
+                                      time__lte=datetime.time(16, 30))
+        boom = bottleExplosion.objects.filter(data=datetime.date.today(),
+                                              time__gte=datetime.time(8),
+                                              time__lte=datetime.time(16, 30))
+
+    elif end1 <= datetime.datetime.now().time() <= end2:
+
+        plan = 27000
+        table = Table5.objects.filter(startdata=datetime.date.today(),
+                                      starttime__gte=datetime.time(16, 29),
+                                      starttime__lte=datetime.time(23, 59))
+        speed = speed5.objects.filter(data=datetime.date.today(),
+                                      time__gte=datetime.time(16, 29),
+                                      time__lte=datetime.time(23, 59))
+        boom = bottleExplosion.objects.filter(data=datetime.date.today(),
+                                              time__gte=datetime.time(16, 29),
+                                              time__lte=datetime.time(23, 59))
+    else:
+        plan = 29000
+        table = Table5.objects.filter(startdata=datetime.date.today(),
+                                      starttime__gte=datetime.time(00, 00),
+                                      starttime__lte=datetime.time(8, 0))
+        speed = speed5.objects.filter(data=datetime.date.today(),
+                                      time__gte=datetime.time(00, 00),
+                                      time__lte=datetime.time(8, 0))
+        boom = bottleExplosion.objects.filter(data=datetime.date.today(),
+                                              time__gte=datetime.time(16, 29),
+                                              time__lte=datetime.time(8, 0))
+
+
+
+
+    try:
+        avgSpeed = round(speed.aggregate(Avg('speed')).get('speed__avg'), 2)
+    except:
+        avgSpeed = 0
+    try:
+        sumProstoy = table.aggregate(Sum('prostoy')).get('prostoy__sum')
+
+        if (sumProstoy == None):
+            sumProstoy = '00:00'
+    except:
+        sumProstoy = '00:00'
+    try:
+        allProc = round((round(speed.aggregate(Sum('speed')).get('speed__sum') / 20, 2)) / plan * 100, 2)
+    except:
+        allProc = 0
+    try:
+        boomOut = boom.aggregate(Sum('bottle')).get('bottle__sum')
+        if (boomOut == None):
+            boomOut = 0
+    except:
+        boomOut = 0
+    result = {"allProc": allProc, "boomOut": boomOut,'sumProstoy':str(sumProstoy),'avgSpeed':avgSpeed}
+    return JsonResponse(result)
 
 
 def update(request):
-
     if request.method == 'POST':
 
         pk = request.POST.get('pk')
@@ -279,6 +308,5 @@ def update(request):
             except:
                 a = Table5(comment=v, id=pk)
                 a.save()
-
 
     return HttpResponse('yes')
